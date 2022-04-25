ab9a38fd8e539bf012fb18b16d991ade8dac253f
Viktor Stoianov <viktor.web.frontend@gmail.com>
2022-04-25 19:54:57 +0300

    feat: add class darken for burger-menu

--- shelter/js/burger-pets.js
+++ shelter/js/burger-pets.js
@@ -8,18 +8,47 @@ const BTN = document.querySelector("body > div > header > nav");
 const MENU = document.querySelector(
   "body > div > header > div.burger-wrapper > div"
 );
+const FOR_DARKEN = document.querySelector("body > div:nth-child(1)");
+const FOR_DARKEN_HEADER = document.querySelector(
+  "body > div.background > header"
+);
+const FOR_DARKEN_LOGO = document.querySelector(
+  "body > div.background > header > div.logo"
+);
 
 BTN.addEventListener("click", () => {
   if (window.matchMedia("(max-width: 767.5px)").matches) {
     // BTN.style.transform = BTN.style.transform === "" ? "rotate(90deg)" : "";
+
     if (BTN.style.transform === "") {
       BTN.style.background = "url(../assets/icons/Burger-main.png)";
       BTN.style.transform = "rotate(90deg)";
       MENU.style.right = "0px";
+      FOR_DARKEN.classList.add("darken");
+      FOR_DARKEN_HEADER.style.background = "rgba(41, 41, 41, 0.6)";
+      FOR_DARKEN_LOGO.style.visibility = "hidden";
       document.querySelector("body").style.overflow = "hidden";
+      if (FOR_DARKEN.classList.contains("darken")) {
+        document
+          .querySelector("body > div.darken")
+          .addEventListener("click", () => {
+            BTN.style.transform = "";
+            MENU.style.right = "-350px";
+            FOR_DARKEN.classList.remove("darken");
+            FOR_DARKEN_HEADER.style.background = "";
+            FOR_DARKEN_LOGO.style.visibility = "";
+
+            document.querySelector("body").style.overflow = "auto";
+            BTN.style.background = "";
+          });
+      }
     } else {
       BTN.style.transform = "";
       MENU.style.right = "-350px";
+      FOR_DARKEN.classList.remove("darken");
+      FOR_DARKEN_HEADER.style.background = "";
+      FOR_DARKEN_LOGO.style.visibility = "";
+
       document.querySelector("body").style.overflow = "auto";
       BTN.style.background = "";
     }
@@ -31,17 +60,24 @@ const LINK_HELP = document.querySelector(
 const LINK_CONTACTS = document.querySelector(
   "body > div > header > div.burger-wrapper > div > nav > a.burger__menu__link-contacts.burger__link"
 );
-LINK_HELP.addEventListener("click", () => {
-  if (window.matchMedia("(max-width: 767.5px)").matches) {
-    MENU.style.right = "-350px";
-    BTN.style.transform = "";
-    document.querySelector("body").style.overflow = "auto";
-  }
-});
+// LINK_HELP.addEventListener("click", () => {
+//   if (window.matchMedia("(max-width: 767.5px)").matches) {
+//     MENU.style.right = "-350px";
+//     BTN.style.transform = "";
+//     FOR_DARKEN.classList.remove("darken");
+//     FOR_DARKEN_HEADER.style.background = "";
+
+//     document.querySelector("body").style.overflow = "auto";
+//   }
+// });
 LINK_CONTACTS.addEventListener("click", () => {
   if (window.matchMedia("(max-width: 767.5px)").matches) {
     MENU.style.right = "-350px";
     BTN.style.transform = "";
+    FOR_DARKEN.classList.remove("darken");
+    FOR_DARKEN_HEADER.style.background = "";
+    FOR_DARKEN_LOGO.style.visibility = "";
+
     document.querySelector("body").style.overflow = "auto";
     // BTN.style.background = "url(../assets/icons/Burger-pets.png)";
     BTN.style.background = "";
--- shelter/js/burger.js
+++ shelter/js/burger.js
@@ -8,17 +8,35 @@ const BTN = document.querySelector("body > div > header > nav");
 const MENU = document.querySelector(
   "body > div > header > div.burger-wrapper > div"
 );
+const FOR_DARKEN = document.querySelector("body > div:nth-child(1)");
+
 BTN.addEventListener("click", () => {
   if (window.matchMedia("(max-width: 767.5px)").matches) {
     // BTN.style.transform = BTN.style.transform === "" ? "rotate(90deg)" : "";
     if (BTN.style.transform === "") {
       BTN.style.transform = "rotate(90deg)";
       MENU.style.right = "0px";
-
+      FOR_DARKEN.classList.add("darken");
       document.querySelector("body").style.overflow = "hidden";
+      if (FOR_DARKEN.classList.contains("darken")) {
+        document
+          .querySelector("body > div.darken")
+          .addEventListener("click", () => {
+            BTN.style.transform = "";
+            MENU.style.right = "-350px";
+            FOR_DARKEN.classList.remove("darken");
+            FOR_DARKEN_HEADER.style.background = "";
+            FOR_DARKEN_LOGO.style.visibility = "";
+
+            document.querySelector("body").style.overflow = "auto";
+            BTN.style.background = "";
+          });
+      }
     } else {
       BTN.style.transform = "";
       MENU.style.right = "-350px";
+      FOR_DARKEN.classList.remove("darken");
+
       document.querySelector("body").style.overflow = "auto";
     }
   }
@@ -34,6 +52,7 @@ LINK_HELP.addEventListener("click", () => {
     MENU.style.right = "-350px";
     BTN.style.transform = "";
     document.querySelector("body").style.overflow = "auto";
+    FOR_DARKEN.classList.remove("darken");
   }
 });
 LINK_CONTACTS.addEventListener("click", () => {
@@ -41,5 +60,6 @@ LINK_CONTACTS.addEventListener("click", () => {
     MENU.style.right = "-350px";
     BTN.style.transform = "";
     document.querySelector("body").style.overflow = "auto";
+    FOR_DARKEN.classList.remove("darken");
   }
 });
