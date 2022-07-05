dcc70b5ddd436b5d7ad78e2a17895fe2f8640a30
Viktor Stoianov <viktor.web.frontend@gmail.com>
2022-07-06 00:23:04 +0300

    feat: add types in news.ts

--- src/components/interfaces-and-types/interfaces-and-type.ts
+++ src/components/interfaces-and-types/interfaces-and-type.ts
@@ -10,3 +10,19 @@ export interface IGetResp {
 }
 
 export type CallbackVoid<T> = (data: T) => void;
+
+export type source = {
+  id: string,
+  name: string
+}
+
+export interface DataNews {
+  author: string;
+  content: string;
+  description: string;
+  publishedAt: string;
+  source: source;
+  title: string;
+  url: string;
+  urlToImage: string;
+}
