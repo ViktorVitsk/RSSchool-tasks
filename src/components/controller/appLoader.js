2a8d90c03938783706b204e8b7a9511cd28f6efc
Viktor Stoianov <viktor.web.frontend@gmail.com>
2022-07-05 18:00:06 +0300

    feat: add apiKey in appLoader.js

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '99b96077bacb4ca3b96ee6c1f3013430', // получите свой ключ https://newsapi.org/
         });
     }
 }
