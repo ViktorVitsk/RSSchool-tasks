46c40c883f74791bf49375bf318f18c392e55162
Viktor Stoianov <viktor.web.frontend@gmail.com>
2022-07-08 19:52:21 +0300

    feat: add svg in footer

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ const baseConfig = {
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
         ],
     },
     resolve: {
