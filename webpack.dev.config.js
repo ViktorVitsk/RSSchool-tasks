2590ba0056519006d98bd2ce20650a18350b1c56
Viktor Stoianov <viktor.web.frontend@gmail.com>
2022-07-16 02:20:26 +0300

    feat: add global styles

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,9 +1,7 @@
-const path = require("path");
-
 module.exports = {
-  mode: "development",
-  devtool: "inline-source-map",
+  mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.resolve(__dirname, "./dist"),
+    static: './',
   },
 };
