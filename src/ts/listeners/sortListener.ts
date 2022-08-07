85932919482bd38c20f651c5a25279e44b62c7bc
Viktor Stoianov <viktor.web.frontend@gmail.com>
2022-08-07 23:20:02 +0300

    fix: resolve problem with sort

--- src/ts/listeners/sortListener.ts
+++ src/ts/listeners/sortListener.ts
@@ -1,19 +1,19 @@
 import Api from '../api/Api';
 import render from '../view/RenderHTML';
 
-export default async (api: Api) => {
-  // const table = document.querySelector('.winners__table');
-  document.querySelector('.winners__table')?.addEventListener('click', async (event) => {
+export default (api: Api) => {
+  document.body.addEventListener('click', async (event) => {
     const target = event.target as EventTarget;
-
-    if (target === document.querySelector('.table__wins')) {
-      await api.setSortOrder('wins');
+    if (target instanceof HTMLElement) {
+      if (target.classList.contains('table__wins')) {
+        await api.setSortOrder('wins');
+      }
+      if (target.classList.contains('table__time')) {
+        await api.setSortOrder('time');
+      }
+      const table = document.querySelector('.table__body') as HTMLElement;
+      const rendWins = render.renderWinnersCars(api.data.winners, api.data.winnersPage);
+      table.innerHTML = rendWins;
     }
-    if (target === document.querySelector('.table__time')) {
-      await api.setSortOrder('time');
-    }
-    const table = document.querySelector('.table__body') as HTMLElement;
-    const rendWins = render.renderWinnersCars(api.data.winners, api.data.winnersPage);
-    table.innerHTML = rendWins;
   });
 };
